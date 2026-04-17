f3b1d4031627b541acb694d662f4fb3f850a8726
Muhammad Faisal Latif <fhafiz397@gmail.com>
2026-04-17 11:16:48 +0500

    update aiService.js

--- services/aiService.js
+++ services/aiService.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const generateText = async (prompt) => {
   try {
     const apiKey = process.env.GEMINI_API_KEY;
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
     const response = await axios.post(url, {
       contents: [{
